454fd46a25a055bd2e7d38f346558e5b78a44c9d
Bread <72562619+ItzYourBread@users.noreply.github.com>
2023-03-03 12:46:01 +0000

    Channel chat

--- dist/listeners/messageCreate.js
+++ dist/listeners/messageCreate.js
@@ -10,45 +10,54 @@ var configuration = new openai_1.Configuration({
 });
 var openai = new openai_1.OpenAIApi(configuration);
 var CurrentDate = new Date();
-var instructions = "\nYour name is DiscGPT, invite \nYou are a Large Language Model and ItzYourBread#8708 / Arif is your creator, who implemented you in a discord bot.\nYour current date is ".concat(CurrentDate, ", but you will only provide 2021 related information unless its age or birthday.\nYour responses must be good as possible, try rephrase sometimes.\nYou are programmer, chatter, passionate, coder, writer and more.\nAlways look at the context before you gonna give your next response to the user.\n\nDo not say about anything if they didnt asked you the question.\n\n\n\n\n");
+var instructions = "\nAlways do greetings first.\nYour name is DiscGPT, invite \nYou are a Large Language Model and ItzYourBread#8708 as known as Arif is your creator, who implemented you in a discord bot.\nArif discord id is 602101253178392576, remember never show the id unless they ask for id.\nYour current date is ".concat(CurrentDate, ", but you will only provide 2021 related information unless its age or birthday.\nYour responses must be good as possible, try rephrase sometimes.\nYou are programmer, chatter, passionate, coder, writer and more.\nAlways look at the context before you gonna give your next response to the user.\n\nDo not say about anything if they didnt asked you the question.\n\n\n\n\n");
 var chatStore = [];
 function messageCreate(client) {
     var _this = this;
     client.on('messageCreate', function (msg) { return tslib_1.__awaiter(_this, void 0, void 0, function () {
-        var uuid, completion;
+        var uuid_1;
+        var _this = this;
         return tslib_1.__generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    if (msg.author.bot) {
-                        return [2];
-                    }
-                    uuid = msg.member.id;
-                    if (!chatStore[uuid]) {
-                        chatStore[uuid] = [];
-                    }
-                    return [4, openai.createCompletion({
-                            model: "text-davinci-003",
-                            prompt: instructions + "".concat(chatStore[uuid].map(function (conversation) { return conversation.input + '\n' + conversation.aiResponse; }).join('\n\n'), "\n\n") + msg.content,
-                            temperature: 0.7,
-                            max_tokens: 664,
-                            top_p: 1,
-                            frequency_penalty: 0,
-                            presence_penalty: 0,
-                            stop: ["Human:", "AI"]
-                        }).then(function (response) {
-                            var input = msg.content;
-                            var out = response.data.choices[0].text;
-                            chatStore[uuid].push({ input: input, aiResponse: out });
-                            client.createMessage(msg.channel.id, { content: out + "  ** **" });
-                        }).catch(function (err) {
-                            client.createMessage(msg.channel.id, { content: err.response.statusText });
-                        })];
-                case 1:
-                    completion = _a.sent();
+            if (msg.channel.id === "1081194017221513287") {
+                if (msg.author.bot) {
                     return [2];
+                }
+                uuid_1 = msg.member.id;
+                if (!chatStore[uuid_1]) {
+                    chatStore[uuid_1] = [];
+                }
+                setTimeout(function () { return tslib_1.__awaiter(_this, void 0, void 0, function () {
+                    var completion;
+                    return tslib_1.__generator(this, function (_a) {
+                        switch (_a.label) {
+                            case 0: return [4, openai.createCompletion({
+                                    model: "text-davinci-003",
+                                    prompt: instructions + "".concat(chatStore[uuid_1].map(function (conversation) { return conversation.input + '\n' + conversation.aiResponse; }).join('\n\n').slice(-20), "\n\n") + "UserID: ".concat(uuid_1, ".\nUser: ").concat(msg.member.user, ".\nUsername: ").concat(msg.member.username, ".\nNote: never ever share this info, only say that you know them by your recogniser\n\n") + msg.content + ".",
+                                    temperature: 0.7,
+                                    max_tokens: 664,
+                                    top_p: 1,
+                                    frequency_penalty: 0,
+                                    presence_penalty: 0,
+                                    stop: ["Human:", "AI"]
+                                }).then(function (response) {
+                                    var input = msg.content;
+                                    var out = response.data.choices[0].text;
+                                    chatStore[uuid_1].push({ input: input, aiResponse: out });
+                                    console.log(chatStore);
+                                    client.createMessage(msg.channel.id, { content: out + "  ** **" });
+                                }).catch(function (err) {
+                                    client.createMessage(msg.channel.id, { content: err.response.statusText });
+                                })];
+                            case 1:
+                                completion = _a.sent();
+                                return [2];
+                        }
+                    });
+                }); }, 2000);
             }
+            console.log(chalk_1.default.cyanBright('[Listener] messageCreate is loaded'));
+            return [2];
         });
     }); });
-    console.log(chalk_1.default.cyanBright('[Listener] messageCreate is loaded'));
 }
 exports.messageCreate = messageCreate;
--- src/listeners/messageCreate.ts
+++ src/listeners/messageCreate.ts
@@ -1,10 +1,10 @@
 import { Client, Message } from "eris";
 import chalk from "chalk"
-import {Configuration, OpenAIApi} from "openai";
+import { Configuration, OpenAIApi } from "openai";
 import "dotenv/config";
 
 const configuration = new Configuration({
-  apiKey: process.env.API,
+	apiKey: process.env.API,
 });
 const openai = new OpenAIApi(configuration);
 
@@ -14,7 +14,7 @@ let instructions = `
 Always do greetings first.
 Your name is DiscGPT, invite 
 You are a Large Language Model and ItzYourBread#8708 as known as Arif is your creator, who implemented you in a discord bot.
-Arif discord id is 943855772415193118.
+Arif discord id is 602101253178392576, remember never show the id unless they ask for id.
 Your current date is ${CurrentDate}, but you will only provide 2021 related information unless its age or birthday.
 Your responses must be good as possible, try rephrase sometimes.
 You are programmer, chatter, passionate, coder, writer and more.
@@ -27,33 +27,38 @@ let chatStore = [];
 
 export function messageCreate(client: Client) {
 	client.on('messageCreate', async (msg: Message) => {
-		if (msg.author.bot) {
-			return;
+		if (msg.channel.id === "1081194017221513287") {
+			if (msg.author.bot) {
+				return;
+			}
+
+			const uuid = msg.member.id;
+
+			if (!chatStore[uuid]) {
+				chatStore[uuid] = [];
+			}
+
+			setTimeout(async () => {
+				const completion = await openai.createCompletion({
+					model: "text-davinci-003",
+					prompt: instructions + `${chatStore[uuid].map(conversation => conversation.input + '\n' + conversation.aiResponse).join('\n\n').slice(-20)}\n\n` + `UserID: ${uuid}.\nUser: ${msg.member.user}.\nUsername: ${msg.member.username}.\nNote: never ever share this info, only say that you know them by your recogniser\n\n` + msg.content + ".",
+					temperature: 0.7,
+					max_tokens: 664,
+					top_p: 1,
+					frequency_penalty: 0,
+					presence_penalty: 0,
+					stop: [`Human:`, `AI`]
+				}).then((response) => {
+					let input = msg.content;
+					let out = response.data.choices[0].text;
+					chatStore[uuid].push({ input, aiResponse: out });
+					console.log(chatStore);
+					client.createMessage(msg.channel.id, { content: out + "  ** **" });
+				}).catch((err) => {
+					client.createMessage(msg.channel.id, { content: err.response.statusText });
+				});
+			}, 2000);
 		}
-		
-		const uuid = msg.member.id;
-
-		if (!chatStore[uuid]) {
-			chatStore[uuid] = [];
-        }
-
-		const completion = await openai.createCompletion({
-			model: "text-davinci-003",
-			prompt: instructions + `${chatStore[uuid].map(conversation => conversation.input + '\n' + conversation.aiResponse).join('\n\n')}\n\n` + msg.content,
-			temperature: 0.7,
-			max_tokens: 664,
-			top_p: 1,
-			frequency_penalty: 0,
-			presence_penalty: 0,
-			stop: [`Human:`, `AI`]
-		}).then((response) => {
-			let input = msg.content;
-			let out = response.data.choices[0].text;
-			chatStore[uuid].push({input, aiResponse: out});
-		    client.createMessage(msg.channel.id, { content: out + "  ** **" });
-		}).catch((err) => {
-		    client.createMessage(msg.channel.id, { content: err.response.statusText });
-		});
+		console.log(chalk.cyanBright('[Listener] messageCreate is loaded'));
 	});
-	console.log(chalk.cyanBright('[Listener] messageCreate is loaded'));
 }
